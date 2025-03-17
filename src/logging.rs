4a371254e9d602cdf8c3e719c47d212214d37c20
1Dice <23101388+1DIce@users.noreply.github.com>
2025-03-17 21:20:44 +0100

    Change log level

--- src/logging.rs
+++ src/logging.rs
@@ -2,7 +2,7 @@ use tracing::Level;
 
 pub fn setup_logging() {
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::WARN)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
